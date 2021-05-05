7d35a24adee8f4a8a450555afeb91d1804f3df31
Kshitijc114 <60551846+Kshitijc114@users.noreply.github.com>
2021-05-05 19:16:11 +0530

    Add files via upload

---
+++ includes/get_user_id.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+    include 'includes/common.php';
+    $email= $_SESSION['email'];
+    //$user_query = "SELECT id FROM users WHERE email = '$email'";
+    $user_query = "SELECT id FROM users WHERE email = '" . $_SESSION['email'] . "'";
+    $result = mysqli_query($con, $user_query)or die($mysqli_error($con));
+		$row = mysqli_fetch_array($result);
+		$user_id = $row['id'];
+?>
